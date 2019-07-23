25ee1573b98cf5a6e1520e2ba549f14fe95eca50
Anton Bugaev <32480392+aabugaev@users.noreply.github.com>
2019-07-24 00:16:04 +0300

    Add files via upload

--- Razmechator_self.py
+++ Razmechator_self.py
@@ -4,6 +4,23 @@
 # In[ ]:
 
 
+import subprocess
+import sys
+
+def install(package):
+    subprocess.call([sys.executable, "-m", "pip", "install", package])
+
+try:
+    import pandas as pd
+    import numpy as np
+    import xlrd
+
+except:
+    install('pandas')
+    install('numpy')
+    install('xlrd')
+
+
 import pandas as pd
 import numpy as np
 from math import isnan
